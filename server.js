b6a888de916f02aa6aa70e7b90c485fdc362fc8b
Jenny Moon <jenny0moon@gmail.com>
2023-03-14 21:06:53 +0000

    Add routes for get & post requests

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
 
-app.listen(3000, () => console.log('Server Started'));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
